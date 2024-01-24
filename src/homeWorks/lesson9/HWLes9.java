b4c34f0b69d71f0394ab8371cf9479d448524222
Zhaaako01 <zhaako01@gmail.com>
2024-01-23 20:56:12 -0500

    another lesson is over, homework is done

---
+++ src/homeWorks/lesson9/HWLes9.java
@@ -0,0 +1,16 @@
+package homeWorks.lesson9;
+
+public class HWLes9 {
+    public static void abc() {
+        String s1 = new String("AAA");
+        String s2 = new String("BBB");
+        String s3 = new String("AABB");
+    }
+
+    public static void main(String[] args) {
+        HWLes9 les9 = new HWLes9();
+        abc();
+        abc();
+        String s1 = new String("ABC");
+    }
+}
---
+++ src/lesson9/Test.java
@@ -0,0 +1,7 @@
+package lesson9;
+
+public class Test {
+    int zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz = 5;
+    double $sd4__tr2 = 13.4;
+    String _$$$$$asd_A_______$$$$$$$;
+}
