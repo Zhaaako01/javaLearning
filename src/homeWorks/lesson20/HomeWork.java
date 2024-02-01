d4608be693af2e62c918ac8f4b8d7ec265cdb040
Zhaaako01 <zhaako01@gmail.com>
2024-02-01 18:51:44 -0500

    another homework is done

--- src/homeWorks/lesson20/HomeWork.java
+++ src/homeWorks/lesson20/HomeWork.java
@@ -1,14 +1,24 @@
 package homeWorks.lesson20;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
+
 
 public class HomeWork {
-   static void abc(String...strings){
+    public static ArrayList<String> abc(String... strings) {
+        ArrayList<String> result = new ArrayList<>();
+        for (String str : strings) {
+            if (!result.contains(str)) {
+                result.add(str);
+            }
+        }
 
-   }
+        Collections.sort(result);
+        System.out.println(result);
+        return result;
+    }
 
     public static void main(String[] args) {
-
+        abc("poka", "okay", "bye", "okay");
     }
 }
